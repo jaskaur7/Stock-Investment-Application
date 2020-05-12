5561cf8a061cfec32b3e426a415de76c3b091ae3
jaskaur7 <64623795+jaskaur7@users.noreply.github.com>
2020-05-12 18:41:27 -0400

    Update SMS.py

--- SMS.py
+++ SMS.py
@@ -12,14 +12,15 @@ class Notification():
     #Recepient number shoud be registered to send SMS
     def sendSms(self,stockName):
         #account_sid and auth_token are user specific and can be retrieved fom twilio website
-        account_sid = 'ACf08c30993e8ecf5239d038dff5902700'
-        auth_token = '9bd9a32bf17b37e3e35665967c5a32fa'
+        account_sid = ''
+        auth_token = ''
         client = Client(account_sid, auth_token)
         textMessage = "You can now buy the "+stockName+" stocks"
         message = client.messages.create(
                              body=textMessage,
-                             from_='+12029522907',
-                             to='+18732886120'
+                  #from twilio number to your personal number
+                             from_='',
+                             to=''
                          )
         # Display this statement in the phone message
         print("Your message has been sent for '{0}' share.".format(stockName))
